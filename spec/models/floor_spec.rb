8a99578c889ef79595d4d35f817fff2ecea8b61b
Jake Miller <jake0miller@gmail.com>
2019-10-29 23:31:13 -0600

    Add belongs to relationship to floor model test

--- spec/models/floor_spec.rb
+++ spec/models/floor_spec.rb
@@ -5,12 +5,10 @@ RSpec.describe Floor, type: :model do
     it {should validate_presence_of :level}
     it {should validate_presence_of :width}
     it {should validate_presence_of :length}
-    it {should validate_presence_of :m_fare}
-    it {should validate_presence_of :c_fare}
-    it {should validate_presence_of :b_fare}
   end
 
   describe 'Relationships' do
+    it {should belong_to :structure}
     it {should have_many :rows}
   end
 end
