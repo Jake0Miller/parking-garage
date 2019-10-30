9e9e916d650b44c52306d8a2c6f113604f4d530f
Jake Miller <jake0miller@gmail.com>
2019-10-30 04:30:22 -0600

    Create base controller with helper methods for checking current user and admin auth level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :admin?
+
+  def current_user
+    @current_user ||= session[:role]
+  end
+
+  def admin?
+    current_user == 'admin'
+  end
 end
