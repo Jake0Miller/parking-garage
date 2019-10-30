3b94bdc8420901dba9097885678019fe53572d36
Jake Miller <jake0miller@gmail.com>
2019-10-29 23:30:52 -0600

    Add test validations for fare and revenue

--- spec/models/structure_spec.rb
+++ spec/models/structure_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe Structure, type: :model do
     it {should validate_presence_of :height}
     it {should validate_presence_of :width}
     it {should validate_presence_of :length}
+    it {should validate_presence_of :m_fare}
+    it {should validate_presence_of :c_fare}
+    it {should validate_presence_of :b_fare}
+    it {should validate_presence_of :m_revenue}
+    it {should validate_presence_of :c_revenue}
+    it {should validate_presence_of :b_revenue}
   end
 
   describe 'Relationships' do
