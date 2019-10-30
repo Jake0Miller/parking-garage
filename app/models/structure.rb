a3a2e421b77c7e6712f93bcf163dab8f09369607
Jake Miller <jake0miller@gmail.com>
2019-10-29 23:30:19 -0600

    Add validations for fare and revenue to structure model

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -1,5 +1,6 @@
 class Structure < ApplicationRecord
   validates_presence_of :height, :width, :length,
-                        :m_fare, :c_fare, :b_fare
+                        :m_fare, :c_fare, :b_fare,
+                        :m_revenue, :c_revenue, :b_revenue
   has_many :floors
 end
