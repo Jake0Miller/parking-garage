bb333513216cb09617935d0db77d4fdb26d2fa1f
Jake Miller <jake0miller@gmail.com>
2019-10-29 23:45:00 -0600

    Add relationship test to spot model

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
   describe 'Validations' do
+    it {should validate_presence_of :number}
     it {should validate_presence_of :width}
   end
 
   describe 'Relationships' do
+    it {should belong_to :row}
     it {should belong_to :vehicle}
   end
 end
