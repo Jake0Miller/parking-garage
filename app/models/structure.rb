d12eccbe72af2b522468f48a9037c5cb53c2b70a
Jake Miller <jake0miller@gmail.com>
2019-10-29 23:28:14 -0600

    Add fare tracking to structure

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -1,4 +1,5 @@
 class Structure < ApplicationRecord
-  validates_presence_of :height, :width, :length
+  validates_presence_of :height, :width, :length,
+                        :m_fare, :c_fare, :b_fare
   has_many :floors
 end
