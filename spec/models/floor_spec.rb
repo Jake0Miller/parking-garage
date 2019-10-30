64ae7e22df17f369d3fbc05eb7f13b2d9cc1f00a
Jake Miller <jake0miller@gmail.com>
2019-10-30 00:02:38 -0600

    Add manual test for validating floor uniqueness

--- spec/models/floor_spec.rb
+++ spec/models/floor_spec.rb
@@ -12,4 +12,22 @@ RSpec.describe Floor, type: :model do
     it {should belong_to :structure}
     it {should have_many :rows}
   end
+
+  describe 'Uniqueness' do
+    it 'cannot have two floors on the same level' do
+      structure = Structure.create!(height: 3, width: 10, length: 10)
+
+      expect(Floor.count).to eq(0)
+
+      structure.floors.create!(level: 1, width: 10, length: 10)
+
+      expect(Floor.count).to eq(1)
+
+      floor = structure.floors.new(level: 1, width: 10, length: 10)
+      expect(floor).to_not be_valid
+      expect(floor.errors[:level]).to include("has already been taken")
+
+      expect(Floor.count).to eq(1)
+    end
+  end
 end
