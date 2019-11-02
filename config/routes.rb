1bb66318b3900fa19a52a909d4e1decb94206ed8
Jake Miller <jake0miller@gmail.com>
2019-11-02 17:29:43 -0600

    Create index resource for structures

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index', as: :root
 
+  resources :structures, only: [:index]
+
   get '/login', to: 'sessions#create', as: :login
   get '/logout', to: 'sessions#destroy', as: :logout
 
