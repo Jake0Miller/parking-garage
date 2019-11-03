980f21a3b77aace35bbf6ce2f48d9ed26cf9feb1
Jake Miller <jake0miller@gmail.com>
2019-11-02 18:07:35 -0600

    Add total structure revenue in admin view

--- spec/features/structures/show_spec.rb
+++ spec/features/structures/show_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Structures show' do
+  include ActionView::Helpers::NumberHelper
+
   before :each do
     @structure_1 = Structure.create!(name: 'Building A',
       height: 5, width: 10, length: 100,
       m_fare: 5, c_fare: 10, b_fare: 25,
-      m_revenue: 0, c_revenue: 0, b_revenue: 0)
+      m_revenue: 100, c_revenue: 150, b_revenue: 125)
   end
 
   it 'I can see a structure as a visitor' do
@@ -36,5 +38,6 @@ RSpec.describe 'Structures show' do
 
     expect(page).to have_content(@structure_1.name)
     expect(page).to have_content("Number of Floors: #{@structure_1.height}")
+    expect(page).to have_content("Total Revenue: #{number_to_currency(@structure_1.total_revenue)}")
   end
 end
