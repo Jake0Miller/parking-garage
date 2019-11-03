bf251fbe66ce297ba05b696158d6ec5ebee72fff
Jake Miller <jake0miller@gmail.com>
2019-11-03 03:19:02 -0700

    Write tests for parking vehicles

---
+++ spec/features/floors/park_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Park vehicles' do
+  before :each do
+    @structure_1 = Structure.create!(name: 'Building A',
+      height: 2, width: 3, length: 10,
+      m_fare: 5, c_fare: 10, b_fare: 25,
+      m_revenue: 100, c_revenue: 150, b_revenue: 125)
+  end
+
+  it 'I can park a motorcycle' do
+    visit root_path
+    click_on 'Parking Visitor'
+    visit structure_path(@structure_1)
+    select '1', :from => "floor[height]"
+    click_on 'View'
+    click_on 'Motorcycle'
+  end
+
+  it 'I can park a motorcycle' do
+    visit root_path
+    click_on 'Parking Visitor'
+    visit structure_path(@structure_1)
+    select '1', :from => "floor[height]"
+    click_on 'View'
+    click_on 'Car'
+  end
+
+  it 'I can park a motorcycle' do
+    visit root_path
+    click_on 'Parking Visitor'
+    visit structure_path(@structure_1)
+    select '1', :from => "floor[height]"
+    click_on 'View'
+    click_on 'Bus'
+  end
+end
