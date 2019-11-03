0da28ae26436e54f62c99c072cb511a0f59e265a
Jake Miller <jake0miller@gmail.com>
2019-11-03 15:07:28 -0700

    Add instance method for rendering an empty parking spot designation type

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,4 +3,10 @@ class Spot < ApplicationRecord
   validates_uniqueness_of :number, scope: :row
   belongs_to :row
   belongs_to :vehicle, optional: true
+
+  def empty_icon
+    return 'M' if self.width == 2
+    return 'C' if self.width == 3
+    return 'L' if self.width == 4
+  end
 end
